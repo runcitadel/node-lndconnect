a55c75f26bc399837bbd30c3e737cf8c59553d19
Aaron Dewes <aaron.dewes@protonmail.com>
2022-02-13 09:33:28 +0100

    Workaround for inconsistent browser bahaviour

--- src/parse.ts
+++ src/parse.ts
@@ -5,10 +5,18 @@ import type { lndconnectUrlData } from './types';
  * @return {Object} Lnd connect data (object containing host, cert, and macaroon keys).
  */
 export default function parse(string: string): lndconnectUrlData {
-  const parsedUrl = new URL(string);
+  let parsedUrl = new URL(string);
+
+  // In browsers, the URL API behaviour isn't consistent if using a custom protocol.
+  // https://felixfbecker.github.io/whatwg-url-custom-host-repro/
+  // Force the protocol to HTTP and parse again to work around this.
+  if(parsedUrl.protocol !== 'lndconnect:')
+    throw new Error('Invalid lndconnect url');
+  
+  parsedUrl.protocol = "http:";
+  parsedUrl = new URL(parsedUrl.toString());
 
   if (
-    parsedUrl.protocol !== 'lndconnect:' ||
     !parsedUrl.searchParams.has('cert') ||
     !parsedUrl.searchParams.has('macaroon')
   ) {
